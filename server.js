667956f1168ba784cca34f070eb43152511e0d16
Ravenn <ravennmb@gmail.com>
2025-02-27 20:42:18 -0700

    completed basic server function

--- server.js
+++ server.js
@@ -10,19 +10,21 @@ app.use(cors());
 //can replace with a db
 const users = [];
 
+//signup endpoint
 app.post("/api/signup", async (req, res)=>{
     try{
         //Checks if username is unique.
         const { username, password } = req.body;
         if(username.find((user)=>user.username === username)){
-            return res.status(400).json(message: "Username unavailable")
+            return res.status(400).json({message: "Username unavailable"})
         }
         //password in the bcrypt hash comes from user post method in const above
         //number is the password legnth without hash value
         const hashedPassword = await bcrypt.hash(password, 12)
         users.push({ username, password: hashedPassword });
+        return res.status(201).json({message: "Signup Complete!"})
     } catch (error) {
-
+        return res.status(500).json({message: "Server ERror", error: error.message})
     }
 })
 
